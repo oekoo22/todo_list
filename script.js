c76c969add1ecc452747bf78e96ce3d56dcd823b
oekoo <y.dykow22@gmail.com>
2022-10-04 11:24:32 +0200

    Put correct brackets to the load arrow function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-window.addEventListener('load', {} => {
+window.addEventListener('load', () => {
     const form = document.querySelector("#todo-form");
     const input = document.querySelector("#todo-input");
     const listElement = document.querySelector("#tasks");
+    
+    console.log(form);
 })
